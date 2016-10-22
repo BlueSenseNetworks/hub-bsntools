55414c198fa105cb4a0d5bcfb91e3cfd41c35623
Nemanja Tosic <nemanja@bluesensenetworks.com>
2016-10-22 17:50:24 +0200

    Read hostname prefix from config file

--- bsntools/firstboot/hostname.py
+++ bsntools/firstboot/hostname.py
@@ -1,15 +1,20 @@
 import re
 import logging
 from subprocess import check_output
+import ConfigParser
 
 
 def check_hostname():
+    config = ConfigParser.ConfigParser()
+    config.read('/etc/bsn/bsn.ini')
+    hostname_prefix = config.get('Hostname', 'prefix')
+
     hosts_file_path = '/etc/hosts'
     current_host_name = check_output('hostname')
     regex = re.compile("^((?:\d{1,3}\.){3}\d{1,3})(\s+)(" + current_host_name + ")$", re.MULTILINE)
 
     serial = file('/proc/cpuinfo').read().split('Serial')[1].strip()[-8:]  # Get last 8 characters of the serial
-    new_host_name = 'bsn-' + serial
+    new_host_name = hostname_prefix + '-' + serial
 
     if new_host_name != current_host_name:
         logging.debug('Hostname mismatch, fixing...')
